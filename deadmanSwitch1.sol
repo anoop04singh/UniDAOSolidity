4110b75819d4746a77910b91a2a52b5786edaf7d
Anoop Singh <48690990+anoop04singh@users.noreply.github.com>
2023-10-06 00:06:19 +0530

    Add files via upload

---
+++ deadmanSwitch1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract deadmanSwitch{
+    address owner;
+    address payable toSend;
+    uint lastBlock;
+    uint check;
+
+    constructor(address payable _toSend) {
+        owner = msg.sender;
+        lastBlock = block.number;
+        toSend = _toSend;
+    }
+
+
+    //function transfer(address payable recepient) public {
+        //require(msg.sender==owner);
+        //uint256 balance = address(this).balance;
+        //recepient.transfer(balance);
+
+    function still_alive() public {
+        require(msg.sender==owner);
+        lastBlock = block.number;
+    }
+    function isDead() public{
+        check = block.number - lastBlock;
+        if(check >= 10){
+        uint256 balance = address(this).balance;
+        toSend.transfer(balance);
+        }
+    
+    }
+    
+    }
+
